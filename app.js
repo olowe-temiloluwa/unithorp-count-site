5b0aeb369bda8d6eabcbdaf51d6135a6997f1caa
olowe temiloluwa <abosedeolowe2@gmail.com>
2022-10-14 12:34:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let deadline = new Date("dec 17, 2022 15:37:25").getTime(); 
+let x = setInterval(function() {   
+    let now = new Date().getTime();
+
+    let distance = deadline - now; 
+
+    let days = Math.floor(distance  / (1000 * 60 * 60 * 24));
+
+    let hours = Math.floor((distance %(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+
+    let minutes = Math.floor((distance  % (1000 * 60 * 60)) / (1000 * 60)); 
+    
+    let seconds = Math.floor((distance  % (1000 * 60)) / 1000); 
+    
+    document.querySelector(".day").innerHTML =days ; 
+    
+    document.querySelector(".hours").innerHTML =hours;
+    
+    document.querySelector(".minute").innerHTML = minutes;  
+    
+    document.querySelector(".seconds").innerHTML =seconds; 
+    
+    }, 1000);
---
+++ throwback/app.js
@@ -0,0 +1,6 @@
+let show = document.querySelectorAll('.play')
+
+let secretNumber = Math.trunc(Math.random() * 9 + 1);
+console.log(secretNumber);
+
+
